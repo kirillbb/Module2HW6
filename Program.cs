a5d05cf285fd15ca1368f3865e97270e37b98775
Kirill Sokolov <kirill.sokolov.work@gmail.com>
2022-02-15 21:00:37 +0200

    add some classes, add class diagram

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Module2HW6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Starter.Run();
+        }
+    }
+}
---
+++ Starter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2HW6
+{
+    internal class Starter
+    {
+        public static void Run()
+        {
+
+        }
+    }
+}
